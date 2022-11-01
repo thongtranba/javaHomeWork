9f14a1b95571b64fd7287afc7563c6720a80c5b0
Ba Thong Tran <82447776+thongtranba@users.noreply.github.com>
2022-11-01 11:18:04 +0100

    Add files via upload
    fibonacciRecursion update

--- src/homeWorkDay8/fibonacciRecursion.java
+++ src/homeWorkDay8/fibonacciRecursion.java
@@ -15,20 +15,18 @@ public class fibonacciRecursion {
 	}
 
 	public static int findFibonacci(int number) { // time complexity -> O(n)
-		int[] fibo = new int[number+1]; //1 step
-		if (number == 0) {	//1step
-			return fibo[0]= 0;
 
-		} else if (number == 1) { //1step
-			return  fibo[1] = 1;
+		if (number == 0) { // 1step
+			return 0;
 
-		} else  {
-			fibo[number] = findFibonacci(number-1)+ findFibonacci(number-2); //n steps
-			
-			return fibo[number]; //1step
+		} else if (number == 1) { // 1step
+			return 1;
+
+		} else {
+
+			return findFibonacci(number - 1) + findFibonacci(number - 2); // n steps;
 		}
 
-		
 	}
 
 }
